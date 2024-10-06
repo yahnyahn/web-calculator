f26a39157c009972691b2da5401883e3c189f99b
yahnyahn <yawningatdawn@gmail.com>
2024-10-06 18:15:14 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const display = document.getElementById("display");
+let calculated = false;
+
+function appendToDisplay(input, isOperator=null){
+
+    delError()
+
+    if (calculated && !isOperator){
+        clearDisplay();
+        calculated = false;
+    }
+    display.value += input;
+
+    if(isOperator){
+        calculated = false;
+    }
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+        calculated = true;
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function delAction(){
+    delError()
+    display.value = display.value.slice(0, -1);
+}
+
+function delError(){
+    if (display.value === "Error"){
+        clearDisplay()
+    }
+}
